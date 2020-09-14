f314e1a58c6798e204760a9b378f9e0f8284be00
Tilson Beattie <BeatTils377@GOTVDSB.CA>
2020-09-14 13:31:16 -0400

    Add project files.

---
+++ part 1 basics/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace part_1_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //string afriend = ("Bill");
+            //string secondfriend = ("charles");
+            //string age = ("13");
+
+            //Console.WriteLine($"hello {afriend} and {secondfriend} you are {age} years old");
+
+            //Console.WriteLine($"the name {afriend} has {afriend.Length} letters");
+            //Console.WriteLine($"the name {secondfriend} has {secondfriend.Length} letters");
+
+
+
+            //string greeting = "       Hello world.      ";
+
+            //Console.WriteLine($"[{greeting}]");
+
+            //string trimgretting = greeting.TrimStart();
+            //Console.WriteLine($"[{trimgretting}]");
+
+            //trimgretting = greeting.TrimEnd();
+            //Console.WriteLine($"[{trimgretting}]");
+
+            //trimgretting = greeting.Trim();
+            //Console.WriteLine($"[{trimgretting}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
